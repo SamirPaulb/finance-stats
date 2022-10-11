273824d56ec5441d8fe17a4e1e7e2e2e1abaccb8
Samir Paul <samir.paul.personal@gmail.com>
2022-10-11 07:10:01 +0000

    Update 2 files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { SpeechProvider } from '@speechly/react-client';
+import React from "react";
+import ReactDOM from "react-dom";
+import { SpeechProvider } from "@speechly/react-client";
 
-import { Provider } from './context/context';
-import App from './App';
-import './index.css';
+import { Provider } from "./context/context";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
-  <SpeechProvider appId="7c4aee08-1073-4a32-b862-ebe1850e0732" language="en-US">
+  <SpeechProvider appId="b54bd161-16b1-42c7-b2bc-6fcc39684c28" language="en-US">
     <Provider>
       <App />
     </Provider>
   </SpeechProvider>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
